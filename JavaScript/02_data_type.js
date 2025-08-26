8a2f992766d45481f7b2f78b6cfa26c9ba3bf726
Md. Niaj Morshed <161250020+Niaj-Morshed-Niloy@users.noreply.github.com>
2025-08-26 23:13:46 +0600

    Add files via upload

---
+++ JavaScript/02_data_type.js
@@ -0,0 +1,31 @@
+console.log("Anonnn");
+
+/*
+String
+Number
+Boolean
+BigInt
+
+undefined
+null
+
+object
+
+symbol    */
+
+// var score =102
+
+let score=102;
+let name= "Anon"
+let isLoggedin= false
+
+// Object:
+let teaTypes=["lemon tea","Orange tea","Milk tea"]
+let user={firstname: "anon", lastname:"tahrim"}
+
+let getScore= score;
+
+console.log(getScore);
+
+
+
