8a866a2ed3f4c77ce8bb42f6d42fdbf5ea6edfec
Md. Niaj Morshed <161250020+Niaj-Morshed-Niloy@users.noreply.github.com>
2025-09-19 00:23:31 +0600

    Add files via upload

---
+++ JavaScript/05_logical.js
@@ -0,0 +1,15 @@
+// ! means reverse
+// && means and
+// || means or
+
+let isLoggedin= true
+let ispaid=false
+
+console.log(isLoggedin && ispaid)
+
+let isemailuser=true
+let isgoogleuser=false
+console.log(isemailuser || isgoogleuser)
+
+
+
