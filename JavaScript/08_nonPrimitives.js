1101eb838e8d0346f7b10174dd74e1b503a81acd
Md. Niaj Morshed <161250020+Niaj-Morshed-Niloy@users.noreply.github.com>
2025-09-22 22:22:25 +0600

    Add files via upload

---
+++ JavaScript/08_nonPrimitives.js
@@ -0,0 +1,33 @@
+let username={
+    firstname:"Anon",
+    isloggedin:true,
+    "nick name":"anonnn",
+
+}
+
+username.firstname="Tahrim"
+username.lastname="rahman"
+
+console.log(username.firstname);
+console.log(username.lastname);
+console.log(username["nick name"]);
+console.log(typeof username);
+
+
+let today= new Date();
+console.log(today);
+
+
+//Array
+
+let heros=["A","B","C",true]
+console.log(heros[0]);
+
+let isvalue=true;
+console.log(1+isvalue);
+let isvaluee=2;
+console.log(isvaluee);
+let isvalueeee="2ac";
+console.log(typeof Number(isvalueeee));
+
+
