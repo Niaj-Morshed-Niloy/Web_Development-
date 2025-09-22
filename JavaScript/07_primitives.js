8c7789d210b633e08d8d2483f9c8d24eef9ca5a9
Md. Niaj Morshed <161250020+Niaj-Morshed-Niloy@users.noreply.github.com>
2025-09-22 22:21:25 +0600

    Add files via upload

---
+++ JavaScript/07_primitives.js
@@ -0,0 +1,37 @@
+let balance=111
+console.log(typeof balance);
+
+let anotherbalance = new Number(111)
+console.log(typeof anotherbalance);
+
+let isactive =true
+let isreallyactive=new Boolean(true)
+console.log(isreallyactive);
+
+let firstname;
+let lastname=null;
+console.log(firstname);
+console.log(lastname);
+
+
+let mystring="hello"
+let mystringone="tahrim"
+let username ="anon"
+
+let oldgreet=mystring+" niaj"
+console.log(oldgreet);
+
+let greetmessage=`helloo ${username}`;
+let demoone=`value is ${2*2}`
+console.log(greetmessage);
+console.log(demoone);
+
+let sm1=Symbol("ANonn");
+let sm2 = Symbol("ANonn");
+console.log(sm1==sm2);
+
+
+
+
+
+
