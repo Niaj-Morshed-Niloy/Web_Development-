3aef33c58cf10ff20d05397fa8f5efdc26c4e7f2
Md. Niaj Morshed <161250020+Niaj-Morshed-Niloy@users.noreply.github.com>
2025-09-23 17:28:26 +0600

    Add files via upload

---
+++ JavaScript/09_condition.js
@@ -0,0 +1,64 @@
+let num1=5
+let num2=8
+console.log("lets go");
+
+
+if(num1>num2) {
+    console.log("num 1 is greater than num2");
+    
+
+}
+else{
+    console.log("Nope,num is not greater");
+    
+
+}
+
+console.log("Done");
+
+// //checking if a string is equal to another string:
+
+let username="chai"
+let anotherusername="chai"
+
+if(username==anotherusername){
+
+    console.log("pick another username");
+}
+
+//checking if a variable is a number or not:
+let score=44
+if(typeof score ==="number"){
+
+    console.log("yep this is a number");
+
+}
+else{
+    console.log("no that is not a num  ");
+    
+}
+
+//checking if a boolean value is true or false:
+
+let isteaready=true
+if(isteaready){
+    console.log("tea is ready");
+    
+}else{
+    console.log("not ready");
+    
+}
+
+//checking if an arry is empty or not:
+
+let items=["item1"]
+// console.log(items.length);
+
+if(items.length==0){
+    console.log("array is empty");
+    
+
+}else{
+    console.log("not empty");
+    
+}
